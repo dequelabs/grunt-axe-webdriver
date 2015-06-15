4239f0f9a3133673ad0dbcd3ce1117b7b9aacb6c
David Sturley <david.sturley@deque.com>
2015-06-15 11:16:21 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/*! aXe-grunt-webdriver
+ * Copyright (c) 2015 Deque Systems, Inc.
+ *
+ * Your use of this Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This entire copyright notice must appear in every copy of this file you
+ * distribute or in any file that contains substantial portions of this source
+ * code.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.initConfig({
+
+		connect: {
+			test: {
+				options: {
+					hostname: '0.0.0.0',
+					port: grunt.option('port') || 9876,
+					base: ['.']
+				}
+			}
+    },
+
+		axe_webdriver: {
+			firefox: {
+				options: {
+					threshold: 2
+				},
+        urls: ['http://localhost:' + (grunt.option('port') || 9876) + '/test/fixtures/document-language.html']
+			},
+      chrome: {
+        options: {
+          browser: 'chrome'
+        },
+        urls: ['http://localhost:' + (grunt.option('port') || 9876) + '/test/fixtures/document-language.html'],
+				dest: 'tmp/gu.json'
+      }
+		}
+
+	});
+
+	grunt.registerTask('example', ['connect', 'axe_webdriver']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['example']);
+
+};
---
+++ tasks/axe-webdriver.js
@@ -0,0 +1,54 @@
+/*! aXe-grunt-webdriver
+ * Copyright (c) 2015 Deque Systems, Inc.
+ *
+ * Your use of this Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This entire copyright notice must appear in every copy of this file you
+ * distribute or in any file that contains substantial portions of this source
+ * code.
+ */
+
+'use strict';
+
+var WebDriver = require('selenium-webdriver'),
+	AxeBuilder = require('axe-webdriverjs'),
+	Promise = require('promise'),
+	path = require('path'),
+  reporter = require('../lib/reporter');
+
+module.exports = function(grunt) {
+	grunt.registerMultiTask('axe_webdriver', 'Grunt plugin for aXe utilizing WebDriverJS', function() {
+		var options = this.options({
+			browser: 'firefox',
+			server: null,
+			threshold: 0
+		});
+
+		var done = this.async();
+		var driver = new WebDriver.Builder()
+			.forBrowser(options.browser)
+			.build();
+
+		var dest = this.data.dest;
+		Promise.all(this.data.urls.map(function(url) {
+			return new Promise(function(resolve, reject) {
+				driver
+					.get(url)
+					.then(function() {
+						new AxeBuilder(driver)
+							.analyze(resolve);
+					});
+			});
+		})).then(function(results) {
+      if (dest) {
+        grunt.file.write(dest, JSON.stringify(results, null, '  '));
+      }
+      var result = reporter(grunt, results, options.threshold);
+			driver.quit();
+			done(result);
+		});
+	});
+
+};
